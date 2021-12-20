2b02ae7f57eedeb7c01d5e467258f71707452e97
BeastieBaiter <62881476+BeastieBaiter@users.noreply.github.com>
2021-12-20 15:28:34 +0000

    Created FT21SenderSR
    Created and finished the FT21SenderSR class

--- tp2/RC2021-tp2/src/Main.java
+++ tp2/RC2021-tp2/src/Main.java
@@ -2,6 +2,10 @@
 public class Main {
 
 	public static void main(String[] args ) {
+<<<<<<< Updated upstream
 		cnss.simulator.Simulator.main(new String[] { "configs/config-2.1.txt"});
+=======
+		cnss.simulator.Simulator.main(new String[] { "configs/config-2.4.txt"});
+>>>>>>> Stashed changes
 	}
 }
